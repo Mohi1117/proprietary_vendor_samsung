09c7259f6d4bf47eccdf1aa8b64e8a3d04864b20
Yukick <mohiuddinkafre@gmail.com>
2018-02-09 17:51:04 +0000

    use oss sensorservice#2

--- j7y17lte/j7y17lte-vendor-blobs.mk
+++ j7y17lte/j7y17lte-vendor-blobs.mk
@@ -116,7 +116,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/j7y17lte/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
     vendor/samsung/j7y17lte/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/j7y17lte/proprietary/lib/libsensorlistener.so:system/lib/libsensorlistener.so \
-#    vendor/samsung/j7y17lte/proprietary/lib/libsensorservice.so:system/lib/libsensorservice.so \
     vendor/samsung/j7y17lte/proprietary/lib/libstainkiller.so:system/lib/libstainkiller.so \
     vendor/samsung/j7y17lte/proprietary/lib/libsynaFpSensorTestNwd.so:system/lib/libsynaFpSensorTestNwd.so \
     vendor/samsung/j7y17lte/proprietary/lib/libuniplugin.so:system/lib/libuniplugin.so \
